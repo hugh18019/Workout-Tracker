50d3f1d419e294d710562b7f28e0a8d3c49c2b69
Huiran Lin <hugh80082@gmail.com>
2021-08-17 02:13:42 -0700

    localhost:3000 route renders correct workout info to home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,68 @@
 const router = require('express').Router();
+const db = require('../models');
+
+let lastSevenWorkoutObj = {};
 
 router.get('/', async (req, res) => {
-  console.log('hit home-route');
-  res.render('home');
+  // console.log('hit home-route');
+  // res.render('home');
+  try {
+    const lastSeven = await db.Workout.aggregate([
+      {
+        $lookup: {
+          from: 'exercises',
+          localField: 'exercises',
+          foreignField: '_id',
+          as: 'lookup-data',
+        },
+      },
+      {
+        $addFields: {
+          total_duration: {
+            $sum: '$lookup-data.duration',
+          },
+        },
+      },
+      {
+        $sort: { date: 1 },
+      },
+      {
+        $limit: 7,
+      },
+    ]);
+
+    await createLastSevenWorkoutObj();
+    await populateLastSevenWorkoutObj(lastSeven);
+
+    console.log(lastSevenWorkoutObj);
+    // res.status(200).json(lastSevenWorkoutObj);
+    res.render('home', { lastSevenWorkoutObj: lastSevenWorkoutObj });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
+function createLastSevenWorkoutObj() {
+  return new Promise((resolve, reject) => {
+    lastSevenWorkoutObj = {
+      lastWorkoutDate: '',
+      totalWorkoutDuration: 0,
+      exercisePerformed: 0,
+    };
+    resolve('Success');
+  });
+}
+
+function populateLastSevenWorkoutObj(lastSeven) {
+  return new Promise((resolve, reject) => {
+    for (let each of lastSeven) {
+      lastSevenWorkoutObj.totalWorkoutDuration += each.total_duration;
+      lastSevenWorkoutObj.exercisePerformed += each.exercises.length;
+    }
+    lastSevenWorkoutObj.lastWorkoutDate = lastSeven[lastSeven.length - 1].date;
+    resolve('Success');
+  });
+}
 
 module.exports = router;
